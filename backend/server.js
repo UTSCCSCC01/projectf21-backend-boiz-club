b8f9e5c54b0993cd2de0f16c1b9e192b15d7e01e
Payam Yektamaram <payamyek@gmail.com>
2021-09-24 00:58:30 -0400

    fix: database functionality

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,34 @@
-const express= require("express");
+const express = require("express");
+const { urlencoded } = require('body-parser');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const username=process.env.DM_USER
-const passwd=process.env.DB_PASSWORD
-const uri = "mongodb+srv://"+username+":"+passwd+"@data.psveh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const username = process.env.DB_USER
+const passwd = process.env.DB_PASSWORD
+const uri = "mongodb+srv://" + username + ":" + passwd + "@data.psveh.mongodb.net/dev_pawsup?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion?.v1 });
+
+let dev_db;
+let test_collection;
+
 client.connect(err => {
-    const dev_db=client.db("dev_pawsup")
-
-	const test_collection = dev_db.collection("test");
-    // perform actions on the collection object
-
+	dev_db = client.db("dev_pawsup")
+	test_collection = dev_db.collection("test");
+	// perform actions on the collection object
 });
-const port =8080;
-const server =express()
 
+const port = 8080;
+const server = express()
 
-server.use(bodyParser.urlencoded({extended: true}));
+server.use(urlencoded({ extended: true }));
 
-server.listen(port, () => {  console.log('We are live on ' + port);});
+server.listen(port, () => { console.log('We are live on ' + port); });
 
-server.get('/health',(req,res)=>{res.send("Endpoint is up and running")});
+server.get('/health', (req, res) => { res.send("Endpoint is up and running") });
 
-server.post('/create',(req,res)=>{
-	test_collection.insertOne(req.body)
-	.catch(error){console.error(error)
-	}});
+server.post('/create', (req, res) => {
+	test_collection.insertOne({"message": "I have been born!"})
+		.catch(error => {
+			console.error(error)
+		})
+	res.send({message: "Item added to database."})
+});
