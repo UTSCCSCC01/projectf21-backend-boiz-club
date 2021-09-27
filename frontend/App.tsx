f1a083c6cf4d3e0001ff31d61f9933fb45d7c2d3
markchen8717 <29904105+markchen8717@users.noreply.github.com>
2021-09-26 21:01:37 -0400

    integrated eslint; path aliasing

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from '@/hooks/useCachedResources';
+import useColorScheme from '@/hooks/useColorScheme';
+import Navigation from '@/navigation';
 
-import store from './stores';
-import { Provider } from 'react-redux'
+import store from '@/stores';
+import {Provider} from 'react-redux';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
