8f4b6994cf0dfb25378befdf134dbb4c03b56a9a
Tanzim66 <tanzim.ahmed@mail.utoronto.ca>
2021-09-29 02:18:04 -0400

    BAC-69 Add and configure eslint module and add Joi module
    Add and configure eslint module that will help us keep coding style consistent.
    Add Joi module to validate payloads.

--- backend/error/api-error-handler.js
+++ backend/error/api-error-handler.js
@@ -6,6 +6,9 @@ module.exports = (err, req, res, next) => {
   if (err instanceof ApiError) {
     res.status(err.code).json({ status: err.code, message: err.message });
     return;
+  } else if (err instanceof SyntaxError) {
+    res.status(400).json({ status: 400, message: "Invalid payload" });
+    return;
   }
   res.status(500).json({ status: 500, message: "Something went wrong" });
 };
--- backend/modules/test/routesTest.js
+++ backend/modules/test/routesTest.js
@@ -1,11 +1,24 @@
 // Routes related to test
 
 const testService = require("./serviceTest");
+const Joi = require("@hapi/joi");
+const ApiError = require("../../error/ApiError");
+
+// Create schema for payload
+const payloadSchema = Joi.object().keys({
+  message: Joi.string().required(),
+});
+
 module.exports = (app) => {
   // Route for post create new test if message in payload is "Dont fail"
   app.post("/test", async (req, res, next) => {
-    const { body } = req;
     try {
+      // Validate payload
+      const { body } = req;
+      const { error } = await payloadSchema.validate(body);
+      if (error) next(ApiError.badRequestError(error.message));
+
+      // Process data
       const result = await testService.testBody(body);
       res.json(result);
     } catch (error) {
