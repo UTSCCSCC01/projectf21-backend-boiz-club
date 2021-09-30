c7886f5dafa909845491f3f6e9625b5804e20d09
StevenWuzz <steven.limantoro@gmail.com>
2021-09-30 15:18:39 +0700

    BAC-32 feat: add 404 error handler

--- backend/error/ApiError.js
+++ backend/error/ApiError.js
@@ -1,13 +1,12 @@
 module.exports = class ApiError {
   /**
    *
-   * @param {Number} code  http status code
    * @param {String} message error message
    * @param {Array} errors express-validator errors array
+   * @return {ApiError}
    */
-  constructor(code, message, errors) {
-    (this.code = code), (this.message = message);
-    this.errors = errors? errors:[];
+  static requestNotFoundError(message, errors) {
+    return new ApiError(404, message, errors);
   }
 
   /**
