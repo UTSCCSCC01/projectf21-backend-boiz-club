cfa023214e7e83fa766346ac9037278ed81d8a15
StevenWuzz <steven.limantoro@gmail.com>
2021-09-30 15:23:54 +0700

    BAC-32 feat: search user email and create OTP

--- backend/modules/forgotPassword/dalForgotPassword.js
+++ backend/modules/forgotPassword/dalForgotPassword.js
@@ -1,46 +1,77 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const ApiError = require('../../error/ApiError');
+const otpGenerator = require('otp-generator');
 
-const OTPSchema = new mongoose.Schema({
-    otp: {
-        type: String,
-        required: true
-    },
-    expiration_time: {
-        type: Date,
-        required: true
-    },
-    verified: {
-        type: Boolean,
-        default: false,
-        required: true
-    },
-    created_at: {
-        type: Date,
-        default: Date.now()
-    },
-    updated_at: {
-        type: Date,
-        default: Date.now()
-    }
+const otpSchema = new mongoose.Schema({
+  otp: {
+    type: String,
+    required: true,
+  },
+  expiration_time: {
+    type: Date,
+    required: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now(),
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
-OTPSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  });
+const userCredentialSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+});
 
-const OTP = mongoose.model('Blog', blogSchema)
+otpSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const OTP = mongoose.model('OTP', otpSchema);
+const UserCredential = mongoose.model(
+    'UserCredential', userCredentialSchema, 'user_credentials');
 
 module.exports = {
-    postOTP: async () => {
-      const otp = new OTP({
-        otp: otp,
-        expiration_time: expiration_time
-      });
-      return otp.save();
-    },
-  };
-  
+  createAndPostOTP: async () => {
+    const otp = otpGenerator.generate(
+        6, {alphabets: false, upperCase: false, specialChars: false});
+    const expirationTime = new Date(new Date().getTime() + 30*60000);
+
+    const newOTP = new OTP({
+      otp: otp,
+      expiration_time: expirationTime,
+    });
+
+    try {
+      const savedOTP = await newOTP.save();
+      return savedOTP;
+    } catch (error) {
+      throw ApiError.badRequestError(`The OTP ${otp.id} cannot be saved`);
+    }
+  },
+
+  searchEmailUser: async (email) => {
+    try {
+      const user = await UserCredential.findOne({email: email});
+      return user;
+    } catch (err) {
+      throw ApiError.requestNotFoundError(
+          `There is no email ${email} in the database`, err);
+    }
+  },
+};
+
