30b493ab5cd58a4d4b6225e9a09fc0948f330d72
StevenWuzz <steven.limantoro@gmail.com>
2021-09-30 15:24:35 +0700

    BAC-32 feat: set-up gmail and email's template

--- backend/modules/forgotPassword/utils/emailConfig.js
+++ backend/modules/forgotPassword/utils/emailConfig.js
@@ -1,30 +1,33 @@
-const nodemailer = require("nodemailer");
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
+const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: "gmail.cpom",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: process.env.EMAIL_LOGIN,
-      pass: process.env.EMAIL_PASSWORD 
-    }
+  host: 'gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_LOGIN,
+    pass: process.env.EMAIL_PASSWORD,
+  },
 });
 
 const emailForgotPassword = (user, otp) => {
-    const from = process.env.EMAIL_LOGIN
-    const to = user.email
-    const subject = "Pawsup Password Reset"
-    const html = `
-    <p>Dear ${user.first_name| user.email},</p>
+  const from = process.env.EMAIL_LOGIN;
+  const to = user.email;
+  const subject = 'Pawsup Password Reset';
+  const html = `
+    <p>Dear ${user.first_name || user.email},</p>
     <p>We have received your password reset request.</p>
     <p>Please submit the following verification altogether with your new password:</p>
-    <p>Verification code: ${otp}
+    <p>Verification code: ${otp}</p>
     <p>If you don’t use code within 30 minutes, it will expire.</p>
 
 
     <p>Best Regards,</p>
     <p>Pawsup Admin Team</p>
-    `
-}
+    `;
+  return {from, to, subject, html};
+};
 
-module.exports = {transporter, emailForgotPassword}
+module.exports = {transporter, emailForgotPassword};
