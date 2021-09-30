394511f6553e9136c8130bea5d5bb8865b467725
StevenWuzz <steven.limantoro@gmail.com>
2021-09-30 15:25:19 +0700

    BAC-32 feat: expose DAL, routes, and services

---
+++ backend/modules/forgotPassword/index.js
@@ -0,0 +1,5 @@
+const forgotPasswordDal = require('./dalForgotPassword');
+const forgotPasswordRoutes = require('./routesForgotPassword');
+const forgotPasswordService = require('./serviceForgotPassword');
+module.exports = {forgotPasswordDal,
+  forgotPasswordRoutes, forgotPasswordService};
