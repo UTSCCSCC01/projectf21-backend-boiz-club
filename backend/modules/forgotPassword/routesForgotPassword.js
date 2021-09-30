5d0e4b8bed7afe162111f3c9b40798e6ddd9bfb3
StevenWuzz <steven.limantoro@gmail.com>
2021-09-30 15:25:57 +0700

    BAC-32: route for forgot password request

---
+++ backend/modules/forgotPassword/routesForgotPassword.js
@@ -0,0 +1,12 @@
+const forgotPasswordService = require('./serviceForgotPassword');
+
+module.exports = (app) => {
+  app.post('/api/v1/reset-password/:email', async (req, res, next) => {
+    try {
+      const result = await forgotPasswordService.sendOTPEmail(req.params.email);
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  });
+};
