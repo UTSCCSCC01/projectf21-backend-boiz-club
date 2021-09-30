697465b83ae21b5198f41cca32818e2bbe82a771
StevenWuzz <steven.limantoro@gmail.com>
2021-09-30 15:19:19 +0700

    BAC-32 feat: add forgotPassword route

--- backend/routes.js
+++ backend/routes.js
@@ -1,7 +1,9 @@
 // All routes
 
 const test = require('./modules/test');
+const forgotPassword = require('./modules/forgotPassword');
 
 module.exports = (app) => {
   test.testRoutes(app);
+  forgotPassword.forgotPasswordRoutes(app);
 };
