b31ad602fb5ca5c9f5e7b80dbd616841a6410878
StevenWuzz <steven.limantoro@gmail.com>
2021-10-01 14:26:53 +0700

    BAC-32 feat: verify transporter and testing code

--- backend/modules/forgotPassword/serviceForgotPassword.js
+++ backend/modules/forgotPassword/serviceForgotPassword.js
@@ -7,9 +7,15 @@ module.exports = {
   sendOTPEmail: async (email) => {
     const user = await forgotPasswordDal.searchEmailUser(email);
     const otpInstance = await forgotPasswordDal.createAndPostOTP();
-
+    /* Only for testing
+    const user = {
+      'email': email,
+    };
+    */
     const emailTemplate = emailForgotPassword(user, otpInstance.otp);
 
+
+    await transporter.verify();
     // How to catch error from the callback function?
     await transporter.sendMail(emailTemplate, (err, info) => {
       if (err) {
@@ -27,5 +33,24 @@ module.exports = {
         return encryptedDetails;
       }
     });
+
+    /*
+    try {
+      await transporter.sendMail(emailTemplate);
+      const details = {
+        'timestamp': otpInstance.created_at,
+        'check': email,
+        'message': 'OTP has been successfully sent to the user',
+        'otp_id': otpInstance.id,
+      };
+
+      const encryptedDetails = CryptoJS.AES.encrypt(
+          JSON.stringify(details), process.env.PASSPHRASE);
+
+      return encryptedDetails;
+    } catch (error) {
+      throw ApiError.badRequestError('Failed to send the email', error);
+    }
+    */
   },
 };
