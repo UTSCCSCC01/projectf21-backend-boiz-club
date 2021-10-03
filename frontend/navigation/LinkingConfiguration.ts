d27dd9385b1db9680d9dcd676dfac1cc9d4d20b8
Tony Chen <tonychen1100@hotmail.com>
2021-10-02 20:11:30 -0400

    Feat (BAC-30): create login UI

--- frontend/navigation/LinkingConfiguration.ts
+++ frontend/navigation/LinkingConfiguration.ts
@@ -7,9 +7,9 @@
 import { LinkingOptions } from '@react-navigation/native';
 import * as Linking from 'expo-linking';
 
-import { RootStackParamList } from '@/types';
+import { HomeStackParamList } from '@/types';
 
-const linking: LinkingOptions<RootStackParamList> = {
+const linking: LinkingOptions<HomeStackParamList> = {
   prefixes: [Linking.makeUrl('/')],
   config: {
     screens: {
