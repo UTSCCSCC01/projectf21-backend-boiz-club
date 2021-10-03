cdb5ec4ae97a74f47543ed75bb75a349fd7d60d1
StevenWuzz <steven.limantoro@gmail.com>
2021-10-03 17:42:38 +0700

    BAC-32 fix: resolve merge conflicts

--- backend/routes.js
+++ backend/routes.js
@@ -1,17 +1,7 @@
 // All routes
-<<<<<<< HEAD
 const routes = require('./api/routes');
 
 module.exports = (app) => {
   routes.testRoutes(app);
   routes.userRoutes(app);
-=======
-
-const test = require('./modules/test');
-const forgotPassword = require('./modules/forgotPassword');
-
-module.exports = (app) => {
-  test.testRoutes(app);
-  forgotPassword.forgotPasswordRoutes(app);
->>>>>>> f75ecb4efdbcf872a70688c147441d606ed66b34
 };
