7fcd24d4e5dd77d4f8f6815ab768dc54a18bf1a5
StevenWuzz <steven.limantoro@gmail.com>
2021-10-03 20:19:58 +0700

    BAC-32 refactor: delete old structures

--- backend/modules/forgotPassword/index.js
+++
@@ -1,5 +0,0 @@
-const forgotPasswordDal = require('./dalForgotPassword');
-const forgotPasswordRoutes = require('./routesForgotPassword');
-const forgotPasswordService = require('./serviceForgotPassword');
-module.exports = {forgotPasswordDal,
-  forgotPasswordRoutes, forgotPasswordService};
