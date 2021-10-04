cd01c85f8f4160c19061085653e1a305d7264617
Tanzim66 <tanzim.ahmed@mail.utoronto.ca>
2021-10-04 10:33:40 -0400

    BAC-76 add get user info endpoint
    Add get user info enpoint to get updated user model for the current logged in user from database. Verification/jwt is required

--- backend/api/routes/routesUser.js
+++ backend/api/routes/routesUser.js
@@ -161,6 +161,24 @@ const uploadGovernmentId = (app) => {
 };
 // End Upload Govrnment ID/Request Verification
 
+// Start get user info
+const getUser = (app) => {
+  app.get(
+      pathPrefix+'/users/self',
+      verifyToken,
+      async (req, res, next) => {
+        const {user} = req;
+        try {
+          const userInfo = await userService.getUser(user.user_id);
+          res.send(userInfo);
+        } catch (error) {
+          next(error);
+        }
+      },
+  );
+};
+// End get user info
+
 module.exports = (app) => {
   // Route for registering a new user
   register(app);
@@ -168,4 +186,6 @@ module.exports = (app) => {
   login(app);
   // Route for uploading government id
   uploadGovernmentId(app);
+  // Route for getting user information
+  getUser(app);
 };
