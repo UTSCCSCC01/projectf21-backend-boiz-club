62f0eecf493ae9ba50cf5e19e190e3852fd4954b
Pasa <pasaali1603@gmail.com>
2021-10-06 01:59:44 +0300

    BAC-28: Implemented Sign-Up Screen - Implemented Sign-Up Screen components - Implemented necessary connections with backend to create a new user.

--- frontend/screens/SignUpScreen.tsx
+++ frontend/screens/SignUpScreen.tsx
@@ -1,12 +1,262 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Box,
+  Button,
+  Heading,
+  Image,
+  FormControl,
+  Input,
+  Link,
+  Text,
+  HStack,
+  VStack,
+} from 'native-base';
 
-const SignUpScreen = () => {
+import { RootStackScreenProps } from '@/types';
+import signup from '@/services/signup';
+
+const SignUpScreen = ({ navigation }: RootStackScreenProps<'SignUp'>) => {
+
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+
+  interface user {
+    email: string;
+    username: string;
+    password: string;
+  }
+
+  const [userData, setUserData] = useState<user>({
+    email: '',
+    username: '',
+    password: '',
+  });
+
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleEmailChange = (input: string) => {
+    setUserData({
+      ...userData,
+      email: input,
+    });
+  };
+
+  const handleUsernameChange = (input: string) => {
+    setUserData({
+      ...userData,
+      username: input,
+    });
+  };
+
+  const handlePasswordChange = (input: string) => {
+    setUserData({
+      ...userData,
+      password: input,
+    });
+  };
+
+  const handleConfirmPasswordChange = (input: string) => {
+    setConfirmPassword(input);
+  };
+
+  // Error struct
+  const [inputError, setInputError] = useState({
+    emailError: false,
+    usernameError: false,
+    passwordError: false,
+    confirmPasswordError: false,
+  });
+
+  // Function to validate user sign-up information
+  const validateInput = (email: string, username: string, password: string, confirmPassword: string) => {
+
+    // Check whether email is entered and satisfies the regex.
+    if (email == null || email.length === 0 || emailRegex.test(email) == false) {
+      setInputError({
+        emailError: true,
+        usernameError: false,
+        passwordError: false,
+        confirmPasswordError: false,
+      });
+    } 
+
+    // Check whether email is entered and satisfies the regex.
+    else if (username == null || username.length === 0) {
+      setInputError({
+        emailError: false,
+        usernameError: true,
+        passwordError: false,
+        confirmPasswordError: false,
+      });
+    } 
+    
+    // Check whether password is entered.
+    else if (password == null || password.length === 0) {
+      setInputError({
+        emailError: false,
+        usernameError: false,
+        passwordError: true,
+        confirmPasswordError: false,
+      });
+    } 
+    
+    // Check whether confirm password is entered and matches password.
+    else if (confirmPassword == null || confirmPassword.length === 0 || confirmPassword !== password) {
+      setInputError({
+        emailError: false,
+        usernameError: false,
+        passwordError: false,
+        confirmPasswordError: true,
+      });
+    } 
+    
+    else {
+      setInputError({
+        emailError: false,
+        usernameError: false,
+        passwordError: false,
+        confirmPasswordError: false,
+      });
+      return true;
+    }
+    return false;
+  };
+
+  const signUpHandle = async (email: string, username: string, password: string, confirmPassword: string) => {
+    if (!validateInput(email, username, password, confirmPassword)) {
+      return;
+    }
+
+    const userSignUp = await signup(email, username, password).catch((err) => {
+      console.log('Sign Up failed');
+      return null;
+    });
+
+    if (userSignUp !== null){
+      console.log('Sign Up Succeed')
+      navigation.navigate("SignIn")
+    }
+
+    return;
+  };
+
   return (
-    <View>
-      <Text>Sign Up Screen PlaceHolder</Text>
-    </View>
+    <Box safeArea flex={1} p="2">
+      <VStack alignItems="center">
+        <Box w="210" h="90">
+          <Image
+            flex={1}
+            source={require('@/assets/images/pawsup-logo.png')}
+            alt="Hello"
+            width={undefined}
+            height={undefined}
+            resizeMode="contain"
+          />
+        </Box>
+        <Heading size="lg" fontSize="3xl">
+          Sign-Up
+        </Heading>
+        <Heading size="sm" fontSize="md" fontWeight="light">
+          Welcome to Pawsup,
+        </Heading>
+        <Heading size="sm" fontSize="md" fontWeight="light">
+          Enter your information to join us!
+        </Heading>
+      </VStack>
+
+      <VStack paddingRight="8" paddingLeft="8" paddingTop="4" space={4}>
+
+        <FormControl isInvalid={inputError.emailError}>
+          <FormControl.Label>Email</FormControl.Label>
+          <Input
+            size="lg"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            placeholder="Email"
+            onChangeText={(text) => handleEmailChange(text)}
+          />
+          <FormControl.ErrorMessage
+            _text={{ fontSize: 'sm', color: 'error.500', fontWeight: 400 }}
+          >
+            Invalid Email
+          </FormControl.ErrorMessage>
+        </FormControl> 
+
+        <FormControl isInvalid={inputError.usernameError}>
+          <FormControl.Label>Username</FormControl.Label>
+          <Input
+            size="lg"
+            keyboardType="default"
+            autoCapitalize="none"
+            placeholder="Username"
+            onChangeText={(text) => handleUsernameChange(text)}
+          />
+          <FormControl.ErrorMessage
+            _text={{ fontSize: 'sm', color: 'error.500', fontWeight: 400 }}
+          >
+            Invalid Username
+          </FormControl.ErrorMessage>
+        </FormControl> 
+
+        <FormControl isInvalid={inputError.passwordError}>
+          <FormControl.Label>Password</FormControl.Label>
+          <Input
+            size="lg"
+            secureTextEntry={true}
+            placeholder="Password"
+            onChangeText={(text) => handlePasswordChange(text)}
+          />
+          <FormControl.ErrorMessage
+            _text={{ fontSize: 'sm', color: 'error.500', fontWeight: 400 }}
+          >
+            Password is Required
+          </FormControl.ErrorMessage>
+        </FormControl>
+
+        <FormControl isInvalid={inputError.confirmPasswordError}>
+          <FormControl.Label>Confirm Password</FormControl.Label>
+          <Input
+            size="lg"
+            secureTextEntry={true}
+            placeholder="Confirm Password"
+            onChangeText={(text) => handleConfirmPasswordChange(text)}
+          />
+          <FormControl.ErrorMessage
+            _text={{ fontSize: 'sm', color: 'error.500', fontWeight: 400 }}
+          >
+            Invalid Confirm password.
+          </FormControl.ErrorMessage>
+        </FormControl>
+
+        <Button
+          height="12"
+          marginTop="0"
+          color="#72BCC1"
+          _text={{ color: 'white', fontSize: 18 }}
+          onPress={() => signUpHandle(userData.email, userData.username, userData.password, confirmPassword)}
+        >
+          Sign Up
+        </Button>
+      </VStack>
+
+      <HStack p="8" space={2} justifyContent="center">
+        <Text fontSize="sm" color="muted.500">
+          Already have an Account?
+        </Text>
+        <Link
+          _text={{
+            color: '#E6973F',
+            fontWeight: 'medium',
+            fontSize: 'sm',
+          }}
+          onPress={() => navigation.navigate('SignIn')}
+        >
+          Sign In
+        </Link>
+      </HStack>
+
+    </Box>
   );
 };
 
 export default SignUpScreen;
+
---
+++ frontend/services/signup.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+// type signup = (email: string, password: string) => string;
+
+async function signup(email: string, username: string, password: string) {
+  const res = await axios
+    .post('https://pawsup-dev-oznda.ondigitalocean.app/api/v1/users/register', {
+      email: email,
+      username: username,
+      password: password,
+    })
+    .catch((err) => {
+      console.log('Post operation for Sign-Up failed.' + err);
+      throw err;
+    });
+}
+
+export default signup;
