f86ecc237a024dcb5f752e3aee0b3d0a05d1f0d5
StevenWuzz <steven.limantoro@gmail.com>
2021-10-06 10:06:49 +0700

    BAC-33 feat: add error handling for 404 code

--- backend/error/ApiError.js
+++ backend/error/ApiError.js
@@ -16,6 +16,17 @@ module.exports = class ApiError {
    * @param {Array} errors express-validator errors array
    * @return {ApiError}
    */
+  static requestNotFoundError(message, errors) {
+    return new ApiError(404, message, errors);
+  }
+
+
+  /**
+   *
+   * @param {String} message error message
+   * @param {Array} errors express-validator errors array
+   * @return {ApiError}
+   */
   static badRequestError(message, errors) {
     return new ApiError(400, message, errors);
   }
