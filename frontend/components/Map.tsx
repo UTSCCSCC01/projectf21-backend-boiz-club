9405123bf81090389475b508d9abd53bfcfb2016
Payam Yektamaram <payamyek@gmail.com>
2021-10-17 13:58:17 -0400

    feat (BAC-93): add circle buffer with prop types

--- frontend/components/Map.tsx
+++ frontend/components/Map.tsx
@@ -1,17 +1,38 @@
 import * as React from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Circle } from 'react-native-maps';
+import { StyleSheet } from 'react-native';
 
-const Map = () => {
+interface Props {
+  lat: number; // latitude of coordinate to render
+  long: number; // longitude of coordinate to render
+  radius?: number; // radius (meters) of circle buffer to draw (default 200 meters)
+}
+
+const Map = ({ lat, long, radius = 200 }: Props) => {
   return (
     <MapView
+      style={styles.map}
       initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        latitude: 43.76055603963581,
+        longitude: -79.40877280352363,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
       }}
-    />
+    >
+      <Circle
+        center={{ latitude: lat, longitude: long }}
+        radius={radius}
+        strokeColor={'rgba(159, 191, 223, 0.5)'}
+        fillColor={'rgba(159, 191, 223, 0.5)'}
+      />
+    </MapView>
   );
 };
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export default Map;
