467726fd84e1b0e7e04b50304cc10a361f426988
pasaaliaslan <pasaali1603@gmail.com>
2021-10-20 18:23:14 +0300

    BAC-85: Backend Connection
    - Implemented createService function for sending service info to backend

---
+++ frontend/services/createService.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+/**
+ * type createService = (service_name: string,
+ *                       service_description: string,
+ *                       service_price: string,
+ *                       contact_number: string,
+ *                       country: string,
+ *                       city: string,
+ *                       postal_code: string,
+ *                       address: string) => string;
+ */
+
+async function createService(
+  service_name: string,
+  service_description: string,
+  service_price: string,
+  contact_number: string,
+  country: string,
+  city: string,
+  postal_code: string,
+  address: string
+) {
+  await axios
+    .post(
+      'https://pawsup-dev-oznda.ondigitalocean.app/api/v1/services/request-verification',
+      {
+        service_name: service_name,
+        service_description: service_description,
+        service_price: service_price,
+        contact_number: contact_number,
+        country: country,
+        city: city,
+        postal_code: postal_code,
+        address: address,
+      }
+    )
+    .catch((err) => {
+      console.log('Post operation for Create Service failed.' + err);
+      throw err;
+    });
+}
+
+export default createService;
