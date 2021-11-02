999ab6a8e5a16f84f5d7e66b88e2b34e7f7b1e0c
Payam Yektamaram <payamyek@gmail.com>
2021-11-01 22:16:01 -0400

    refactor (BAC-129): use env var for path

--- frontend/services/services.ts
+++ frontend/services/services.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
+import Constants from 'expo-constants';
+
+// @ts-ignore
+const { BACKEND_ENDPOINT } = Constants.manifest?.extra;
 
 export async function getVerifiedServices() {
   return axios
-    .get(
-      'https://pawsup-dev-oznda.ondigitalocean.app/api/v1/services/verified?limit=0&skip=0'
-    )
+    .get(`${BACKEND_ENDPOINT}/api/v1/services/verified?limit=0&skip=0`)
     .then((resp) => resp.data)
     .catch((err) => {
       console.log('Get operation for verified services failed. ' + err);
