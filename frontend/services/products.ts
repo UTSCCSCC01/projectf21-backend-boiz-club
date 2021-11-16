e5249e009744188b24f3ce1bf7f277eccb551bf1
markchen8717 <29904105+markchen8717@users.noreply.github.com>
2021-11-16 09:32:45 -0500

    BAC-141 feat: Integrated backend endpoint

--- frontend/services/products.ts
+++ frontend/services/products.ts
@@ -1,27 +1,13 @@
 import axios from 'axios';
 
 export async function getProducts() {
-  return [
-    {
-      _id: '1',
-      product_name: 'Product 1',
-      product_description: 'Description 1',
-      product_price: 123,
-    },
-    {
-      _id: '12',
-      product_name: 'Product 2',
-      product_description: 'Description 2',
-      product_price: 321,
-    },
-  ];
-  //   return axios
-  //     .get(
-  //       'https://pawsup-dev-oznda.ondigitalocean.app/api/v1/services/verified?limit=0&skip=0'
-  //     )
-  //     .then((resp) => resp.data)
-  //     .catch((err) => {
-  //       console.log('Get operation for verified services failed. ' + err);
-  //       throw err;
-  //     });
+  return axios
+    .get(
+      'https://pawsup-dev-oznda.ondigitalocean.app/api/v1/products?limit=0&skip=0'
+    )
+    .then((resp) => resp.data)
+    .catch((err) => {
+      console.log('Get operation for products failed. ' + err);
+      throw err;
+    });
 }
